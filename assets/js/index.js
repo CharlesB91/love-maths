a1d4d38d554e06d81b50ed369ff068719364d8af
CharlesB91 <charles_ballantyne@hotmail.co.uk>
2021-08-13 11:30:02 +0100

    structure added to js file

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,15 @@
+function runGame() {}
 
+function checkAnswer() {}
+
+function calculateAnswer() {}
+
+function incrementScore() {}
+
+function incrementWrongAnswer() {}
+
+function additionQuestion() {}
+
+function subtractionQuestion() {}
+
+function multiplyQuestion() {}
